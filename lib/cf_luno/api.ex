aefead658b49bb764faa31a9eb5d7a96e6c4ec4b
Ernst Roux <ernstroux@gmail.com>
2019-11-12 15:50:00 +0000

    add mock functions at top of luno api

--- lib/cf_luno/api.ex
+++ lib/cf_luno/api.ex
@@ -3,6 +3,32 @@ defmodule CfLuno.Api do
 
   @luno_url_v1 "https://api.mybitx.com/api/1"
 
+  #---------------------------------------------------------------------------------------------------------------------
+  # Mock api
+  #---------------------------------------------------------------------------------------------------------------------
+  def post_order(pair, type, volume, price, post_only) when is_binary(pair) do
+    vol_str = :erlang.float_to_binary(volume, [{:decimals, 6}])
+    price_str = to_string(price)
+    Logger.info("Mock Limit " <> type <> " for " <> vol_str <> " at " <> price_str)
+    path = "/postorder?pair=" <> pair <> "&type=" <> type <> "&volume=" <> vol_str <> "&price=" <> price_str <> "&post_only=" <> post_only
+    Logger.debug("private api v1 post url: #{inspect path}")
+    {:ok, %{"order_id" => "TestOrderID"}}
+  end
+
+  def stop_order(order_id, price) when is_binary(order_id) do
+    Logger.info("Mock cancel limit order #{inspect order_id} at #{inspect price}")
+    path = "/stoporder?order_id=" <> order_id
+    Logger.debug("private api v1 post url: #{inspect path}")
+    {:ok, %{"success" => true}}
+  end
+
+  def list_trades(_params) do
+    {:ok, %{"trades" => [%{"type" => "ASK", "volume" => "0.0005"}, %{"type" => "BID", "volume" => "0.0005"}]}}
+  end
+
+  #---------------------------------------------------------------------------------------------------------------------
+  # API
+  #---------------------------------------------------------------------------------------------------------------------
   @doc """
     Coinbase is used as an oracle price
   """
@@ -33,15 +59,6 @@ defmodule CfLuno.Api do
     |> invoke_private_api_v1_get()
   end
 
-  #def post_order(pair, type, volume, price, post_only) when is_binary(pair) do
-  #  vol_str = :erlang.float_to_binary(volume, [{:decimals, 6}])
-  #  price_str = to_string(price)
-  #  Logger.info("Mock Limit " <> type <> " for " <> vol_str <> " at " <> price_str)
-  #  path = "/postorder?pair=" <> pair <> "&type=" <> type <> "&volume=" <> vol_str <> "&price=" <> price_str <> "&post_only=" <> post_only
-  #  Logger.debug("private api v1 post url: #{inspect path}")
-  #  {:ok, %{"order_id" => "TestOrderID"}}
-  #end
-
   def post_order(pair, type, volume, price, post_only) do
     vol_str = :erlang.float_to_binary(volume, [{:decimals, 6}])
     price_str = to_string(price)
@@ -51,12 +68,6 @@ defmodule CfLuno.Api do
   end
 
   def stop_order(order_id), do: stop_order(order_id, "unknown")
-  #def stop_order(order_id, price) when is_binary(order_id) do
-  #  Logger.info("Mock cancel limit order #{inspect order_id} at #{inspect price}")
-  #  path = "/stoporder?order_id=" <> order_id
-  #  Logger.debug("private api v1 post url: #{inspect path}")
-  #  {:ok, %{"success" => true}}
-  #end
   def stop_order(order_id, price) when is_binary(order_id) do
     Logger.info("Cancel limit order #{inspect order_id} at #{inspect price}")
     "/stoporder?order_id=" <> order_id
@@ -67,11 +78,7 @@ defmodule CfLuno.Api do
     {:ok, %{"success" => true}}
   end
 
-  #def list_trades(_params) do
-  #  {:ok, %{"trades" => [%{"type" => "ASK", "volume" => "0.0005"}, %{"type" => "BID", "volume" => "0.0005"}]}}
-  #end
-
-  def list_trades(params) do
+  def list_trades([pair: _] = params) do
     query_str = URI.encode_query(params)
     "/listtrades?" <> query_str
     |> invoke_private_api_v1_get()
@@ -122,6 +129,6 @@ defmodule CfLuno.Api do
   end
 
   defp get_auth_args() do
-    {:ok, System.get_env("api_key"), System.get_env("api_secret")}
+    {:ok, System.get_env("luno_api_key"), System.get_env("luno_api_secret")}
   end
 end
