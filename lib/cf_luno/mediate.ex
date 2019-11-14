10d033e71ebfa8edc41fa43bb9ea525c1081a5ab
Ernst Roux <ernst.roux@mastercard.com>
2019-11-14 14:25:37 +0000

    move balance, stop and post orders to mediation modules

--- lib/cf_luno/mediate.ex
+++ lib/cf_luno/mediate.ex
@@ -2,17 +2,35 @@ defmodule CfLuno.Mediate do
   require Logger
   import String, only: [to_float: 1]
 
-  def get_bal(asset) do
+  def get_ticker(pair) do
+    {:ok, ticker} = CfLuno.Api.get_ticker(pair)
+    ticker
+  end
+
+  def get_avail_bal(asset) do
     {:ok, %{"balance" => [%{"balance" => avail_bal, "unconfirmed" => unconf_bal, "reserved" => reserved}]}} =
-    CfLuno.Api.balance(asset)
+      CfLuno.Api.balance(asset)
     avail_bal = to_float(avail_bal) + to_float(unconf_bal) - to_float(reserved)
-    Logger.info("Available #{inspect asset} balance: #{inspect avail_bal}")
+    Logger.info("Available #{asset} balance: #{avail_bal}")
     avail_bal
   end
 
   def get_orderbook(pair) do
-  {:ok, orderbook} = CfLuno.Api.get_orderbook_top(pair)
-  orderbook
+    {:ok, orderbook} = CfLuno.Api.get_orderbook_top(pair)
+    orderbook
+  end
+
+  def post_order(pair, type, volume, price, post_only) do
+    vol_str = :erlang.float_to_binary(volume, [{:decimals, 6}])
+    Logger.info("Limit #{type} for #{vol_str} at #{price}")
+    params = [pair: pair, type: type, volume: vol_str, price: price, post_only: post_only]
+    {:ok, %{"order_id" => new_order_id}} = CfLuno.Api.post_order(params)
+    new_order_id
+  end
+
+  def stop_order(order_id, price) do
+    Logger.info("Cancel limit order #{order_id} at #{price}")
+    CfLuno.Api.stop_order(order_id)
   end
 
   def list_open_orders(pair) do
