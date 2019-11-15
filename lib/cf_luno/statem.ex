84b4bad639842a3d6267946d31022474bcd460a3
Ernst Roux <ernst.roux@mastercard.com>
2019-11-15 16:56:59 +0000

    create post order body, convert price to float

--- lib/cf_luno/statem.ex
+++ lib/cf_luno/statem.ex
@@ -256,12 +256,12 @@ defmodule CfLuno.Statem do
   defp calc_limit_order_price(0, _curr_limit_vol, type, %{med_mod: med_mod, pair: pair}) do
     %{"bid" => bid, "ask" => ask} = med_mod.get_ticker(pair)
     Logger.info("Bid price:" <> bid <> " ask price:" <> ask)
-    {bid_int, _rem_bin} = Integer.parse(bid)
-    {ask_int, _rem_bin} = Integer.parse(ask)
+    {bidf, _rem_bin} = Float.parse(bid)
+    {askf, _rem_bin} = Float.parse(ask)
     if type == "ASK" do
-      {:ok, calc_best_price(ask_int, ask_int, bid_int, type)}
+      {:ok, calc_best_price(askf, askf, bidf, type)}
     else
-      {:ok, calc_best_price(bid_int, bid_int, ask_int, type)}
+      {:ok, calc_best_price(bidf, bidf, askf, type)}
     end
   end
   defp calc_limit_order_price(pre_vol, curr_vol, type, %{order_price: curr_price, med_mod: req_mod, pair: pair}) do
@@ -272,7 +272,7 @@ defmodule CfLuno.Statem do
       {0, curr_vol},
       fn (%{"volume" => volume_str, "price" => price_str}, {acc_volume, curr_vol}) ->
         volume = to_float(volume_str)
-        {price, _rem_bin} = Integer.parse(price_str)
+        {price, _rem_bin} = Float.parse(price_str)
         {new_acc_volume, rem_limit_vol} =
           if curr_vol > 0 and ((type == "ASK" and price >= curr_price) or (type == "BID" and price <= curr_price)) do
             new_acc_volume = acc_volume + volume - curr_vol
@@ -288,9 +288,9 @@ defmodule CfLuno.Statem do
             "Best price:" <> best_price <> ", volume:" <> best_vol
             <> ". Best alt price:" <> best_alt_price <> ", volume:" <> best_alt_vol
           )
-          {best_price_int, _} = Integer.parse(best_price)
-          {best_alt_price_int, _} = Integer.parse(best_alt_price)
-          new_price = calc_best_price(best_price_int, price, best_alt_price_int, type)
+          {best_pricef, _} = Float.parse(best_price)
+          {best_alt_pricef, _} = Float.parse(best_alt_price)
+          new_price = calc_best_price(best_pricef, price, best_alt_pricef, type)
           {:halt, {:ok, new_price}}
         else
           {:cont, {new_acc_volume, rem_limit_vol}}
