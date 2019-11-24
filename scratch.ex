144e8bec50e6343c3e2f71c1a863c758ee79f5dc
Ernst Roux <ernstroux@gmail.com>
2019-11-24 10:50:04 +0000

    scratch

--- scratch.ex
+++ scratch.ex
@@ -6,7 +6,7 @@ DynamicSupervisor.start_child(CfBot.DynSup,{CfBot.Statem, %{name: CfCb, med_mod:
 DynamicSupervisor.start_child(CfBot.DynSup,{CfBot.Statem,%{name: CfLuno, med_mod: CfLuno.Mediate, pair: "XBTZAR", ref_pair: "BTC-USD", min_incr: 1, review_time: 2000, dt_pct: 0.002, ut_pct: 0.002, stable_pct: 0.0005}})
 CfBot.Statem.set_mode(CfLuno,"normal")
 
-CfBot.Statem.set_hodl_amt(CfLuno, "primary", 0.5139)
+CfBot.Statem.set_hodl_amt(CfLuno, "primary", 0.634)
 CfBot.Statem.set_sell_amt(CfLuno,0.0)
 CfBot.Statem.set_mode(CfLuno,"hodl")
 
@@ -14,5 +14,7 @@ CfBot.Statem.set_hodl_amt(CfCb, "secondary", 0.0)
 CfBot.Statem.set_buy_amt(CfCb,0.0)
 CfBot.Statem.set_mode(CfCb,"buy")
 
+CfBot.Statem.set_buy_amt(CfCb,0.33448492)
+
 c("lib/cf_cb/mediate.ex")
-c("lib/cf_cb/test_med.ex")
+c("lib/cf_luno/mediate.ex")
