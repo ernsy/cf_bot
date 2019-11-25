edd8552b58bfd77ee2c6967ba9428d301f53c47b
Ernst Roux <ernst.roux@mastercard.com>
2019-11-25 14:04:20 +0000

    scratch

--- scratch.ex
+++ scratch.ex
@@ -4,17 +4,19 @@ DynamicSupervisor.start_child(CfBot.DynSup,{CfBot.Statem, %{name: CfCb, med_mod:
 
 DynamicSupervisor.start_child(CfBot.DynSup,{CfBot.Statem,%{name: CfLuno, med_mod: CfLuno.Mediate, pair: "XBTZAR", ref_pair: "BTC-USD", min_incr: 1, review_time: 2000, dt_pct: 0.002, ut_pct: 0.002, stable_pct: 0.0005}})
 CfBot.Statem.set_mode(CfLuno,"normal")
-
-CfBot.Statem.set_hodl_amt(CfLuno, "primary", 0.78110492)
+CfBot.Statem.set_hodl_amt(CfLuno, "primary", 0.69343897)
 CfBot.Statem.set_sell_amt(CfLuno,0.0)
+
 CfBot.Statem.set_mode(CfLuno,"hodl")
 
 DynamicSupervisor.start_child(CfBot.DynSup,{CfBot.Statem, %{name: CfCb, med_mod: CfCb.Mediate, pair: "BTC-GBP", ref_pair: "BTC-USD", min_incr: 0.01, review_time: 50, dt_pct: 0.0005, ut_pct: 0.0005, stable_pct: 0.0001}})
+CfBot.Statem.set_mode(CfCb,"wait")
 CfBot.Statem.set_hodl_amt(CfCb, "secondary", 0)
 CfBot.Statem.set_buy_amt(CfCb,0.0)
+
 CfBot.Statem.set_mode(CfCb,"buy")
 
-CfBot.Statem.set_buy_amt(CfCb,0.1)
+CfLuno.Mediate.get_avail_bal("XBT")
 
 c("lib/cf_cb/mediate.ex")
 c("lib/cf_luno/mediate.ex")
