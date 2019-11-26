e02367c045fa2fea221526ba7f8eac441aa8ff0d
Ernst Roux <ernst.roux@mastercard.com>
2019-11-26 15:42:48 +0000

    add cc module

---
+++ lib/cf_bot/cc.ex
@@ -0,0 +1,67 @@
+defmodule CfBot.CC do
+
+  def start_luno(hodl_amt) do
+    prep_luno(hodl_amt)
+    set_luno_hodl_mode()
+  end
+
+  def prep_luno(hodl_amt) do
+    DynamicSupervisor.start_child(
+      CfBot.DynSup,
+      {
+        CfBot.Statem,
+        %{
+          name: CfLuno,
+          med_mod: CfLuno.Mediate,
+          pair: "XBTZAR",
+          ref_pair: "BTC-USD",
+          min_incr: 1,
+          review_time: 2000,
+          dt_pct: 0.002,
+          ut_pct: 0.002,
+          stable_pct: 0.0005
+        }
+      }
+    )
+    CfBot.Statem.set_mode(CfLuno, "wait")
+    CfBot.Statem.set_hodl_amt(CfLuno, "primary", hodl_amt)
+    CfBot.Statem.set_sell_amt(CfLuno, 0.0)
+  end
+
+  def set_luno_hodl_mode() do
+    CfBot.Statem.set_mode(CfLuno, "hodl")
+  end
+
+  def start_cb() do
+    prep_cb()
+    set_cb_buy_mode()
+  end
+
+  def prep_cb() do
+    DynamicSupervisor.start_child(
+      CfBot.DynSup,
+      {
+        CfBot.Statem,
+        %{
+          name: CfCb,
+          med_mod: CfCb.Mediate,
+          pair: "BTC-GBP",
+          ref_pair: "BTC-USD",
+          min_incr: 0.01,
+          review_time: 50,
+          dt_pct: 0.0005,
+          ut_pct: 0.0005,
+          stable_pct: 0.0001
+        }
+      }
+    )
+    CfBot.Statem.set_mode(CfCb, "wait")
+    CfBot.Statem.set_hodl_amt(CfCb, "secondary", 0)
+    CfBot.Statem.set_buy_amt(CfCb, 0.0)
+  end
+
+  def set_cb_buy_mode do
+    CfBot.Statem.set_mode(CfCb,"buy")
+  end
+
+end
--- scratch.ex
+++ scratch.ex
@@ -4,21 +4,21 @@ DynamicSupervisor.start_child(CfBot.DynSup,{CfBot.Statem, %{name: CfCb, med_mod:
 
 
 
-DynamicSupervisor.start_child(CfBot.DynSup,{CfBot.Statem,%{name: CfLuno, med_mod: CfLuno.Mediate, pair: "XBTZAR", ref_pair: "BTC-USD", min_incr: 1, review_time: 2000, dt_pct: 0.002, ut_pct: 0.002, stable_pct: 0.0005}})
-CfBot.Statem.set_mode(CfLuno,"normal")
-CfBot.Statem.set_hodl_amt(CfLuno, "primary", 0.69343897)
-CfBot.Statem.set_sell_amt(CfLuno,0.0)
-
-
-
-CfBot.Statem.set_mode(CfLuno,"hodl")
-
 DynamicSupervisor.start_child(CfBot.DynSup,{CfBot.Statem, %{name: CfCb, med_mod: CfCb.Mediate, pair: "BTC-GBP", ref_pair: "BTC-USD", min_incr: 0.01, review_time: 50, dt_pct: 0.0005, ut_pct: 0.0005, stable_pct: 0.0001}})
 CfBot.Statem.set_mode(CfCb,"wait")
 CfBot.Statem.set_hodl_amt(CfCb, "secondary", 0)
 CfBot.Statem.set_buy_amt(CfCb,0.0)
 
 CfBot.Statem.set_mode(CfCb,"buy")
+
+
+
+DynamicSupervisor.start_child(CfBot.DynSup,{CfBot.Statem,%{name: CfLuno, med_mod: CfLuno.Mediate, pair: "XBTZAR", ref_pair: "BTC-USD", min_incr: 1, review_time: 2000, dt_pct: 0.002, ut_pct: 0.002, stable_pct: 0.0005}})
+CfBot.Statem.set_mode(CfLuno,"normal")
+CfBot.Statem.set_hodl_amt(CfLuno, "primary", 0.69343897)
+CfBot.Statem.set_sell_amt(CfLuno,0.0)
+
+CfBot.Statem.set_mode(CfLuno,"hodl")
 
 
 
